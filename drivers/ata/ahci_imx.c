6446173b9229c232e62133271a53eb0f42fe0de7
Marcel Ziswiler <marcel.ziswiler@toradex.com>
2018-10-31 12:35:12 +0100

    ata: imx: add external clock support for i.MX8QM
    Add external reference clock via clock tree. This allows to model
    the shared reference clock provided via PCIE_SATA_REFCLK100M_P/N
    properly.
    
    This is analogous to what we did for PCIe in commit db22e7590338
    ("PCI: imx6: add external clock support for i.MX8QM").
    
    Signed-off-by: Marcel Ziswiler <marcel.ziswiler@toradex.com>
    (cherry picked from commit 4b2d05c05c9e775abf7218ad7a4b52c2d220dd14)
    
    Conflicts:
    	drivers/ata/ahci_imx.c
    Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>
    (cherry picked from commit da0aa53df04d82eb15cecab90d98e6696d882923)
    (cherry picked from commit 52683295a0ad72808f997be5548ec592828c61a1)
    (cherry picked from commit 23dcfab4d4736617a82205b8fb3561d72b4001fa)

--- drivers/ata/ahci_imx.c
+++ drivers/ata/ahci_imx.c
@@ -144,6 +144,7 @@ struct imx_ahci_priv {
 	struct clk *per_clk4;
 	struct clk *per_clk5;
 	void __iomem *phy_base;
+	struct clk *sata_ext;
 	int clkreq_gpio;
 	struct regmap *gpr;
 	bool no_device;
@@ -910,9 +911,13 @@ static int imx_sata_enable(struct ahci_host_priv *hpriv)
 	if (ret)
 		return ret;
 
-	ret = clk_prepare_enable(imxpriv->sata_ref_clk);
+	ret = clk_prepare_enable(imxpriv->sata_ext);
 	if (ret < 0)
 		goto disable_regulator;
+
+	ret = clk_prepare_enable(imxpriv->sata_ref_clk);
+	if (ret < 0)
+		goto disable_ext;
 
 	if (imxpriv->type == AHCI_IMX6Q || imxpriv->type == AHCI_IMX6QP) {
 		/*
@@ -957,6 +962,8 @@ static int imx_sata_enable(struct ahci_host_priv *hpriv)
 
 disable_clk:
 	clk_disable_unprepare(imxpriv->sata_ref_clk);
+disable_ext:
+	clk_disable_unprepare(imxpriv->sata_ext);
 disable_regulator:
 	ahci_platform_disable_regulators(hpriv);
 
@@ -995,6 +1002,8 @@ static void imx_sata_disable(struct ahci_host_priv *hpriv)
 	}
 
 	clk_disable_unprepare(imxpriv->sata_ref_clk);
+
+	clk_disable_unprepare(imxpriv->sata_ext);
 
 	ahci_platform_disable_regulators(hpriv);
 }
@@ -1477,6 +1486,12 @@ static int imx_ahci_probe(struct platform_device *pdev)
 	if (IS_ERR(imxpriv->sata_clk)) {
 		dev_err(dev, "can't get sata clock.\n");
 		return PTR_ERR(imxpriv->sata_clk);
+	}
+
+	imxpriv->sata_ext = devm_clk_get(dev, "sata_ext");
+	if (IS_ERR(imxpriv->sata_ext)) {
+		dev_err(dev, "can't get sata_ext clock.\n");
+		return PTR_ERR(imxpriv->sata_ext);
 	}
 
 	imxpriv->sata_ref_clk = devm_clk_get(dev, "sata_ref");
