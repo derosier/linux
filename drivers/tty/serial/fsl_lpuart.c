719c3e69f508d0e27469b6ba644463ab8720ce00
Philippe Schenker <philippe.schenker@toradex.com>
2019-10-10 19:02:09 +0200

    tty: serial: lpuart: Disable transmitter on setting modem register
    Signed-off-by: Philippe Schenker <philippe.schenker@toradex.com>
    (cherry picked from commit 37dd601da761c15609dfa33d3d665c55ea57c8fa)
    
    Conflicts:
    	drivers/tty/serial/fsl_lpuart.c
    Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>

--- drivers/tty/serial/fsl_lpuart.c
+++ drivers/tty/serial/fsl_lpuart.c
@@ -1425,6 +1425,48 @@ static void lpuart_dma_rx_free(struct uart_port *port)
 	sport->dma_rx_cookie = -EINVAL;
 }
 
+static void __maybe_unused lpuart_setup_rs485(struct lpuart_port *sport)
+{
+	/* Todo implement for 8-bit registers */
+}
+
+static void lpuart32_setup_rs485(struct lpuart_port *sport)
+{
+	struct serial_rs485 *rs485 = &sport->port.rs485;
+	unsigned long val, ctrl, ctrl_saved;
+
+	val = lpuart32_read(&sport->port, UARTMODIR) &
+			~(UARTMODIR_TXRTSPOL | UARTMODIR_TXRTSE);
+
+	/* Make sure transmitter is disabled */
+	ctrl = lpuart32_read(&sport->port, UARTCTRL);
+	ctrl_saved = ctrl;
+	ctrl &= ~(UARTCTRL_TIE | UARTCTRL_TCIE | UARTCTRL_TE |
+			UARTCTRL_RIE | UARTCTRL_RE);
+	lpuart32_write(&sport->port, ctrl, UARTCTRL);
+
+	if (rs485->flags & SER_RS485_ENABLED) {
+		/* Enable auto RS-485 RTS mode */
+		val |= UARTMODIR_TXRTSE;
+
+		/*
+		* The hardware defaults to RTS logic HIGH while transfer.
+		* Switch polarity in case RTS shall be logic HIGH
+		* after transfer.
+		* Note: UART is assumed to be active high.
+		*/
+		if (rs485->flags & SER_RS485_RTS_ON_SEND)
+			val &= ~UARTMODIR_TXRTSPOL;
+		else if (rs485->flags & SER_RS485_RTS_AFTER_SEND)
+			val |= UARTMODIR_TXRTSPOL;
+	}
+
+	lpuart32_write(&sport->port, val, UARTMODIR);
+
+	/* Restore cr2 */
+	lpuart32_write(&sport->port, ctrl_saved, UARTCTRL);
+}
+
 static int lpuart_config_rs485(struct uart_port *port,
 			struct serial_rs485 *rs485)
 {
@@ -1482,19 +1524,12 @@ static int lpuart32_config_rs485(struct uart_port *port,
 	struct lpuart_port *sport = container_of(port,
 			struct lpuart_port, port);
 
-	unsigned long modem = lpuart32_read(&sport->port, UARTMODIR)
-				& ~(UARTMODIR_TXRTSPOL | UARTMODIR_TXRTSE);
-	lpuart32_write(&sport->port, modem, UARTMODIR);
-
 	/* clear unsupported configurations */
 	rs485->delay_rts_before_send = 0;
 	rs485->delay_rts_after_send = 0;
 	rs485->flags &= ~SER_RS485_RX_DURING_TX;
 
 	if (rs485->flags & SER_RS485_ENABLED) {
-		/* Enable auto RS-485 RTS mode */
-		modem |= UARTMODIR_TXRTSE;
-
 		/*
 		 * RTS needs to be logic HIGH either during transer _or_ after
 		 * transfer, other variants are not supported by the hardware.
@@ -1507,23 +1542,14 @@ static int lpuart32_config_rs485(struct uart_port *port,
 		if (rs485->flags & SER_RS485_RTS_ON_SEND &&
 				rs485->flags & SER_RS485_RTS_AFTER_SEND)
 			rs485->flags &= ~SER_RS485_RTS_AFTER_SEND;
-
-		/*
-		 * The hardware defaults to RTS logic HIGH while transfer.
-		 * Switch polarity in case RTS shall be logic HIGH
-		 * after transfer.
-		 * Note: UART is assumed to be active high.
-		 */
-		if (rs485->flags & SER_RS485_RTS_ON_SEND)
-			modem &= ~UARTMODIR_TXRTSPOL;
-		else if (rs485->flags & SER_RS485_RTS_AFTER_SEND)
-			modem |= UARTMODIR_TXRTSPOL;
 	}
 
 	/* Store the new configuration */
 	sport->port.rs485 = *rs485;
 
-	lpuart32_write(&sport->port, modem, UARTMODIR);
+	/* config_rs485 gets called in irqsave context so do not lock again */
+	lpuart32_setup_rs485(sport);
+
 	return 0;
 }
 
@@ -1845,6 +1871,7 @@ static void lpuart32_hw_setup(struct lpuart_port *sport)
 	lpuart_rx_dma_startup(sport);
 	lpuart_tx_dma_startup(sport);
 
+	lpuart32_setup_rs485(sport);
 	lpuart32_setup_watermark_enable(sport);
 	lpuart32_configure(sport);
 
@@ -2767,6 +2794,7 @@ static int lpuart_probe(struct platform_device *pdev)
 	struct device_node *np = pdev->dev.of_node;
 	struct lpuart_port *sport;
 	struct resource *res;
+	unsigned long flags;
 	int ret;
 
 	sport = devm_kzalloc(&pdev->dev, sizeof(*sport), GFP_KERNEL);
@@ -2884,7 +2912,9 @@ static int lpuart_probe(struct platform_device *pdev)
 	    sport->port.rs485.delay_rts_after_send)
 		dev_err(&pdev->dev, "driver doesn't support RTS delays\n");
 
+	spin_lock_irqsave(&sport->port.lock, flags);
 	sport->port.rs485_config(&sport->port, &sport->port.rs485);
+	spin_unlock_irqrestore(&sport->port.lock, flags);
 
 	sport->dma_tx_chan = dma_request_slave_channel(sport->port.dev, "tx");
 	if (!sport->dma_tx_chan)
