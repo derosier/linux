1331ee2b2d4a224388349a62ae0ca472d701d83e
Steve deRosier <derosier@cal-sierra.com>
2020-07-26 16:06:22 -0700

    Revert "crypto: ablkcipher - remove deprecated and unused ablkcipher support"
    This reverts commit d63007eb954e425f45766482813738f41db9af45.
    
     Conflicts:
    	include/crypto/algapi.h
    	include/linux/crypto.h

--- include/crypto/skcipher.h
+++ include/crypto/skcipher.h
@@ -241,6 +241,9 @@ static inline struct skcipher_alg *crypto_skcipher_alg(
 
 static inline unsigned int crypto_skcipher_alg_ivsize(struct skcipher_alg *alg)
 {
+	if (alg->base.cra_ablkcipher.encrypt)
+		return alg->base.cra_ablkcipher.ivsize;
+
 	return alg->ivsize;
 }
 
@@ -283,6 +286,9 @@ static inline unsigned int crypto_skcipher_blocksize(
 static inline unsigned int crypto_skcipher_alg_chunksize(
 	struct skcipher_alg *alg)
 {
+	if (alg->base.cra_ablkcipher.encrypt)
+		return alg->base.cra_blocksize;
+
 	return alg->chunksize;
 }
 
