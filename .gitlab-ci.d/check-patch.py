6f73903d685a476bbf8bd70bd795b993b4efef60
Oleksandr Suvorov <oleksandr.suvorov@toradex.com>
2021-04-09 12:18:16 +0300

    gitlab-ci: check-patch: fix remote branch issue
    If check-patch.pl raises an exception, it may leave the remote source
    "check-patch" undeleted. And all further calls on the repo will fail.
    
    Fix this issue, adding the PID modifier to the repote source name and
    making removing this source mandatory.
    
    Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@toradex.com>

--- .gitlab-ci.d/check-patch.py
+++ .gitlab-ci.d/check-patch.py
@@ -17,34 +17,40 @@ if len(sys.argv) >= 2:
 
 cwd = os.getcwd()
 reponame = os.path.basename(cwd)
+if len(sys.argv) >= 3:
+    reponame = sys.argv[2]
+
 repourl = "https://gitlab.int.toradex.com/rd/%s/%s.git" % (namespace, reponame)
 masterbranch = "toradex_5.4-2.3.x-imx"
+check_patch_branch = "check-patch-%d" % os.getpid()
+rc = 0
 
 # GitLab CI environment does not give us any direct info about the
 # base for the user's branch. We thus need to figure out a common
 # ancestor between the user's branch and current git master.
-subprocess.check_call(["git", "remote", "add", "check-patch", repourl])
-subprocess.check_call(["git", "fetch", "check-patch", masterbranch],
-                      stdout=subprocess.DEVNULL,
-                      stderr=subprocess.DEVNULL)
+subprocess.check_call(["git", "remote", "add", check_patch_branch, repourl])
+try:
+    subprocess.check_call(["git", "fetch", check_patch_branch, masterbranch],
+                          stdout=subprocess.DEVNULL,
+                          stderr=subprocess.DEVNULL)
 
-ancestor = subprocess.check_output(["git", "merge-base",
-                                    "check-patch/" + masterbranch, "HEAD"],
-                                    universal_newlines=True)
+    ancestor = subprocess.check_output(["git", "merge-base",
+                                        check_patch_branch + "/" + masterbranch, "HEAD"],
+                                        universal_newlines=True)
 
-ancestor = ancestor.strip()
+    ancestor = ancestor.strip()
 
-subprocess.check_call(["git", "remote", "rm", "check-patch"])
+    errors = False
 
-errors = False
+    print("\nChecking all commits since %s...\n" % ancestor)
 
-print("\nChecking all commits since %s...\n" % ancestor)
+    ret = subprocess.run(["scripts/checkpatch.pl", "-g", ancestor + "..."])
 
-ret = subprocess.run(["scripts/checkpatch.pl", "-g", ancestor + "..."])
+    if ret.returncode != 0:
+        print(ret)
+        print("    ❌ FAIL one or more commits failed scripts/checkpatch.pl")
+        rc = 1
+finally:
+    subprocess.check_call(["git", "remote", "rm", check_patch_branch])
 
-if ret.returncode != 0:
-    print(ret)
-    print("    ❌ FAIL one or more commits failed scripts/checkpatch.pl")
-    sys.exit(1)
-
-sys.exit(0)
+sys.exit(rc)
